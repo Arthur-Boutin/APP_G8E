8752612cf7dab3c76af9dcfb5a3310a73d5eebb1
Arthur-Boutin <arthurboutin2004@gmail.com>
2025-04-18 08:01:24 +0000

    test responsive 2

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const hamburger = document.querySelector(".hamburger");
+    const mobileMenu = document.querySelector(".mobile-menu");
+
+    hamburger.addEventListener("click", () => {
+      mobileMenu.classList.toggle("active");
+    });
+  });
+</script>
