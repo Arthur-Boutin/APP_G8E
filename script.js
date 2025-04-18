3eb0ec4f5c91412b996d27435d1a773450b6c373
Arthur-Boutin <arthurboutin2004@gmail.com>
2025-04-18 08:21:38 +0000

    modif js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
-  document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector(".hamburger");
     const mobileMenu = document.querySelector(".mobile-menu");
-
+  
     // Vérifiez si les éléments existent avant d'ajouter des événements
     if (hamburger && mobileMenu) {
       hamburger.addEventListener("click", () => {
