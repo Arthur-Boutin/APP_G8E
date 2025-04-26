49e8f16beebbf01ccc6412ee8d5de92e9dcf3a3d
DENG Guokuang <qiongsijieke76@gmail.com>
2025-04-26 20:29:21 +0200

    change

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Éléments hamburger ou mobileMenu introuvables !");
     }
   });
+
+
+// FAQ toggle
+const questionHeaders = document.querySelectorAll('.question-header');
+
+questionHeaders.forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const rightArrow = header.querySelector('.right-arrow');
+        const toggleIcon = header.querySelector('.toggle-icon');
+
+        // Afficher le contenu en mode expansion/réduction
+        content.style.display = content.style.display === 'block' ? 'none' : 'block';
+
+        // Rotation de la flèche
+        if (content.style.display === 'block') {
+            rightArrow.classList.add('rotate');
+            toggleIcon.src = "icons/icons8-chevron-up-50.png"; // Élargir vers le haut
+        } else {
+            rightArrow.classList.remove('rotate');
+            toggleIcon.src = "icons/icons8-chevron-down-50.png"; // Fermé en cas de fermeture
+        }
+    });
+});
