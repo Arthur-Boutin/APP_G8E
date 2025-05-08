cdf0845db022c901150fb00857ed9af2b133fa13
Arthur-Boutin <arthurboutin2004@gmail.com>
2025-05-08 15:52:22 +0200

    sessions

--- Gestion_Utilisateurs.php
+++ Gestion_Utilisateurs.php
@@ -1,21 +1,24 @@
 <?php
-if (!isset($_SESSION['isAdmin']) || !$_SESSION['isAdmin']) {
+session_start();
+
+// Vérifie si l'utilisateur est connecté et est un administrateur
+if (!isset($_SESSION['user']) || $_SESSION['user']['role'] !== 'administrateur') {
     header('Location: index.php');
     exit();
 }
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nutwork - Accueil</title>
+    <title>Nutwork - Gestion des utilisateurs</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
 <!-- Header intégré -->
 <?php include 'header.php'; ?>
-
 
 <main>
     <section class="gestion-utilisateurs">
