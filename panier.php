d676db4a07c2de7e719808560fdffbed24006c92
Arthur-Boutin <arthurboutin2004@gmail.com>
2025-05-09 09:32:04 +0200

    change

--- panier.php
+++ panier.php
@@ -8,19 +8,21 @@
 </head>
 <body>
 <?php
-include
+/*
+// Connexion à la base de données
 $host = 'localhost';
 $dbname = 'app_g8e'; // Nom de votre base de données
 $username = 'root'; // Nom d'utilisateur par défaut pour phpMyAdmin
 $password = ''; // Mot de passe par défaut pour phpMyAdmin
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion à la base de données : " . $e->getMessage());
-}
+  die("Erreur de connexion à la base de données : " . $e->getMessage());
+}*/
 ?>
+
     <!-- Header intégré -->
     <?php include 'header.php'; ?>
   <main>
