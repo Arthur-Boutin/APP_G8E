be2dd94e6731437a6bdc93fce19133cb264f4fa0
DENG Guokuang <qiongsijieke76@gmail.com>
2025-05-23 14:52:59 +0200

    CHANGE

--- messagerie.php
+++ messagerie.php
@@ -77,7 +77,7 @@
 
 <!-- ============================ FOOTER =================================== -->
 <footer class="site-footer">
-  <div><h4>À propos de Nutwork</h4><p><a href="./contact.php">Contactez-nous</a></p><p>À propos de nous</p><p>Blog</p><p><a href="./faq.php">FAQ</a></p></div>
+  <div><h4>À propos de Nutwork</h4><p><a href="./contact.php">Contactez-nous</a></p><p>À propos de nous</p><p><a href="./contact.php">Blog</p><p><a href="./faq.php">FAQ</a></p></div>
   <div><h4>CGU</h4><p><a href="./Mentions.php">Mentions</a></p><p><a href="./cgv.php">CGV</a></p><p>Développement</p></div>
   <div><h4>Aide & Contacts</h4><p>contact@nutwork.com</p><p>28 Rue Notre Dame des Champs, Paris</p></div>
 </footer>
