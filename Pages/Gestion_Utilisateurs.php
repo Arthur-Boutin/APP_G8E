b25d00689fb33e6d06e3777e65b9b3f34560fc85
NVBIL <160732248+NVBIL@users.noreply.github.com>
2025-05-25 22:31:53 +0200

    Update Gestion_Utilisateurs.php

--- Pages/Gestion_Utilisateurs.php
+++ Pages/Gestion_Utilisateurs.php
@@ -96,7 +96,7 @@ try {
                         <td><?php echo htmlspecialchars($u['statutConnexion']); ?></td>
                         <td>
                             <a href="modifier-utilisateur.php?id=<?php echo urlencode($u['idUtilisateur']); ?>" class="btn-modify">Modifier</a>
-                            <a href="Gestion_Utilisateurs.php?id=<?php echo urlencode($u['idUtilisateur']); ?>" class="btn-delete" onclick="return confirm('Supprimer cet utilisateur ?');">Supprimer</a>
+                            <a href="supprimer-utilisateur.php?id=<?php echo urlencode($u['idUtilisateur']); ?>" class="btn-delete" onclick="return confirm('Supprimer cet utilisateur ?');">Supprimer</a>
                         </td>
                     </tr>
                 <?php endforeach; ?>
