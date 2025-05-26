ad8f231d25ebe188dc81e94becc93403442ab4d9
BrendowDevX <helton.nascimento-rodrigues-barbosa@eleve.isep.fr>
2025-05-26 15:25:33 +0200

    Dernier update panier.php
    Modification finale

--- Pages/panier.php
+++ Pages/panier.php
@@ -5,12 +5,15 @@ $host = 'localhost';
 $dbname = 'app_g8e'; // Nom de votre base de données
 $username = 'root'; // Nom d'utilisateur par défaut pour phpMyAdmin
 $password = ''; // Mot de passe par défaut pour phpMyAdmin
-
 try {
   $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
+if (!isset($_SESSION['user'])) {
+    header('Location: panier.php?id=user');
+    exit();
 }
 ?>
 <!DOCTYPE html>
@@ -22,6 +25,9 @@ try {
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+     <script src="PanierSupprimer.js">
+        supprimer();
+        </script>
     <!-- Header intégré -->
     <?php include 'header.php'; ?>
   <main>
@@ -30,6 +36,7 @@ try {
       <table class="panierachat">
       <thead>
         <tr>
+          <th class='tete' scope="col">Description</th>
           <th class='tete' scope="col">Produit</th>
           <th class='tete' scope="col" >Quantité</th>
           <th class='tete' scope="col">Total</th>
@@ -39,15 +46,30 @@ try {
       <tbody>
       <?php
       /*PRODUITS MIS DANS LE PANIER*/
-      $sql="SELECT produit.nom, panierachat.quantite, (produit.prix*panierachat.quantite) AS total FROM panierachat, produit WHERE produit.nProduit=panierachat.idProduit";
+      $id=filter_input(INPUT_GET,'idProduit',FILTER_SANITIZE_NUMBER_INT);
+      if(!empty($id)) {
+        $select_id="DELETE FROM panierachat WHERE idProduit = :idProduit";
+        $stmt = $pdo->prepare($select_id);
+        $stmt->bindParam(':idProduit', $id);
+        if($stmt->execute()) {
+           $return=['Erreur' => false, 'msg' => '<div role>Produit supprimé.</div>'];
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $return = ['Erreur' => false, 'delete' => $row];
+      }else{
+        $return=["'Erreur' => true, 'msg' => '<div role ='alert'>Produit non supprimé"];
+      }
+      echo json_decode($return);
+    }
+      $sql="SELECT produit.description, panierachat.idProduit, produit.nom, panierachat.quantite, (produit.prix*panierachat.quantite) AS total FROM panierachat, produit WHERE produit.nProduit=panierachat.idProduit";
       $stmt = $pdo->prepare($sql);
       $stmt->execute();
       while( $row = $stmt->fetch(PDO::FETCH_ASSOC) ){
         echo "<tr>";
-        echo "<th class='valeurs'>".$row['nom']."</th>" ;
+        echo "<th class='description'>".$row['description']."</th>" ;
+        echo "<th class='valeurs'>".$row['nom']."<br></th>" ;
         echo "<th class='valeurs'>".$row['quantite']." pcs</th>";
         echo "<th class='valeurs'>".$row['total']." €</th>";
-        echo "<th class='supprimer'><a href='panier.php?id='>Supprimer</th>";
+        echo "<th><form method='POST'><input type='button' id='$id' value=Supprimer class='supprimer' onclick='supprimer($row)'></button></th>";
         echo "</tr>";
       }
       /*TOTAL*/
@@ -62,12 +84,17 @@ try {
       </tbody>
       </table>
       <style>
+        .cart-title-bar{
+          width: 200px;
+        }
         .supprimer{
           background-color:black;
           border-top-right-radius: 5pc;
           border-bottom-right-radius: 5pc;
           color: white;
-          width: 9px;
+          width: 200px;
+          height: 50px;
+          font-weight: bold;
         }
         .total{
           background-color:black;
@@ -84,9 +111,11 @@ try {
           color:white;
           padding-left: 80px;
           padding-right: 80px;
+          height: 20px;
         }
-        thead{
-          margin-left: 10px;
+        .cart-container{
+          margin-left: 100px;
+          align-items: center;
         }
         th{
           align-items: center;
@@ -94,18 +123,25 @@ try {
           padding:5px;
           width: 5px;
         }
+        .description{
+          font-weight: normal;
+          background-color:rgba(233, 170, 111, 0.69);
+          color:black;
+          height: 20px;
+        }
         table{
-          padding: 20px;
+          padding: 30px;
           font-size:17px;
           border-radius:2pc;
           background-color: white;
           height: 550px;
-          width: 750px;
-          margin: 5px
+          width: 400px;
+          position: ;
         }
         .valeurs{
           background-color:rgba(233, 170, 111, 0.69);
-          color:black
+          color:black;
+          height: 20px;
         }
         .basdepage{
           background-color: #e9aa6f;
